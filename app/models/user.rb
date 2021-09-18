312cdf9fd1cca03427414167379b9ffe2f536c52
dai.mugi <dxinchuan@gmail.com>
2021-09-18 11:08:03 +0000

    user show update1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
+  validates :introduction, length: { maximum: 100 }
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
   has_secure_password
+  
   mount_uploader :image, ImageUploader
   
   has_many :messages, dependent: :destroy
