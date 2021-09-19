3044e47b4a02d577c7a3cfccc4cc0b20dcf85acc
dai.mugi <dxinchuan@gmail.com>
2021-09-19 10:55:37 +0000

    delete migration-file for heroku

--- db/migrate/20210901021905_remove_clerk_from_rooms.rb
+++
@@ -1,5 +0,0 @@
-class RemoveClerkFromRooms < ActiveRecord::Migration[5.2]
-  def change
-    remove_foreign_key :clerks
-  end
-end
--- db/migrate/20210901022844_records.rb
+++
@@ -1,6 +0,0 @@
-class Records < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :clerks
-    fail ActiveRecord::IrreversibleMigration
-  end
-end
