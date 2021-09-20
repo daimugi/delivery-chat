da8276fc62d49c144643cb1b58f6035c1a7d71c9
dai.mugi <dxinchuan@gmail.com>
2021-09-20 09:44:24 +0000

    room show update5

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,7 +5,7 @@ class MessagesController < ApplicationController
   def create
     if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
       @message = Message.create(params.require(:message).permit(:user_id, :content, :room_id, :image).merge(user_id: current_user.id))
-    else 
+    else
       flash[:alert] = 'メッセージが送信できませんでした'
     end 
     redirect_to "/rooms/#{@message.room_id}"
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,21 @@
 class Message < ApplicationRecord
-  # validates :content, presence: true
+  
+  
+  # with_options presence: true, if: :empty do
+  # validates :content
+  # validates :image
+  # end
+  
   belongs_to :user
   belongs_to :room
   mount_uploader :image, ImageUploader
+  
+  # def empty 
+  #   @message = Message.find_by(content: '', image: '')
+  #   if !@message.content? && !@message.image?
+  #     flash[:alert] = 'メッセージが送信できませんでした'
+  #   end  
+  # end
+  
+  
 end
