760efed99994f3aa829e5c22cbe055c95b15617e
dai.mugi <dxinchuan@gmail.com>
2021-09-21 04:10:52 +0000

    user-name validation update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: { maximum: 20 }
   validates :introduction, length: { maximum: 100 }
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
