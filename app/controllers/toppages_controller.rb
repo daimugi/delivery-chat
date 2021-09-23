69befe493ca7a73b50e4813d86bcfa81cb63947a
dai.mugi <dxinchuan@gmail.com>
2021-09-23 07:01:17 +0000

    user show update4

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -4,6 +4,7 @@ class ToppagesController < ApplicationController
     @users = User.order(id: :desc).page(params[:page]).per(20)
     if logged_in?
       @posts = Post.order(id: :desc).page(params[:page])
+      @posts_price = Post.order(price: :desc).page(params[:page])
     end  
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,30 @@ class UsersController < ApplicationController
     @users = User.order(id: :desc).page(params[:page]).per(20)
     @users.each do |user|
       unless current_user.id == user.id 
-        @user = user
+        lat1 = current_user.latitude
+        lng1 = current_user.longitude
+        lat2 = user.latitude
+        lng2 = user.longitude
+        
+        x1 = lat1.to_f * Math::PI / 180
+        y1 = lng1.to_f * Math::PI / 180
+        x2 = lat2.to_f * Math::PI / 180
+        y2 = lng2.to_f * Math::PI / 180
+        
+        radius = 6378.137
+        
+        diff_y = (y1 - y2).abs
+        
+        calc1 = Math.cos(x2) * Math.sin(diff_y)
+        calc2 = Math.cos(x1) * Math.sin(x2) - Math.sin(x1) * Math.cos(x2) * Math.cos(diff_y)
+        
+        numerator = Math.sqrt(calc1 ** 2 + calc2 ** 2)
+        
+        denominator = Math.sin(x1) * Math.sin(x2) + Math.cos(x1) * Math.cos(x2) * Math.cos(diff_y)
+        
+        degree = Math.atan2(numerator, denominator)
+        
+        @distance = "#{(degree * radius).round(1)} km"
       end
     end  
   end
@@ -45,13 +68,6 @@ class UsersController < ApplicationController
     if @user.save
       flash[:successs] = 'ユーザーを登録しました'
       redirect_to user_path(@user)
-      # @user = User.find(params[:id])
-      # @current_user = @user 
-      # if logged_in? 
-      #   @current_user = User.find(params[:id])
-      # end  
-      # render :show
-      # redirect_to root_url
     else 
       flash.now[:danger] = 'ユーザーの登録に失敗しました'
       render :new
