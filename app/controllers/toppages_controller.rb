dc9aa620b5a8c7bd97c2b209c2ac29f9f69885f8
dai.mugi <dxinchuan@gmail.com>
2021-09-24 14:07:49 +0000

    toppage update3

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,10 +1,11 @@
 class ToppagesController < ApplicationController
   
   def index
-    @users = User.order(id: :desc).page(params[:page]).per(20)
+    @users = User.order(id: :desc).page(params[:page]).per(8)
     if logged_in?
       @posts = Post.order(id: :desc).page(params[:page])
       @posts_price = Post.order(price: :desc).page(params[:page])
+      # @posts_address = Post.order(@distance: :desc).page(param[:page])
     end  
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,35 +5,6 @@ class UsersController < ApplicationController
   
   
   def index
-    @users = User.order(id: :desc).page(params[:page]).per(16)
-    @users.each do |user|
-      unless current_user == user 
-        lat1 = current_user.latitude
-        lng1 = current_user.longitude
-        lat2 = user.latitude
-        lng2 = user.longitude
-        
-        x1 = lat1.to_f * Math::PI / 180
-        y1 = lng1.to_f * Math::PI / 180
-        x2 = lat2.to_f * Math::PI / 180
-        y2 = lng2.to_f * Math::PI / 180
-        
-        radius = 6378.137
-        
-        diff_y = (y1 - y2).abs
-        
-        calc1 = Math.cos(x2) * Math.sin(diff_y)
-        calc2 = Math.cos(x1) * Math.sin(x2) - Math.sin(x1) * Math.cos(x2) * Math.cos(diff_y)
-        
-        numerator = Math.sqrt(calc1 ** 2 + calc2 ** 2)
-        
-        denominator = Math.sin(x1) * Math.sin(x2) + Math.cos(x1) * Math.cos(x2) * Math.cos(diff_y)
-        
-        degree = Math.atan2(numerator, denominator)
-        
-        @distance = "#{(degree * radius).round(1)} km"
-      end  
-    end    
   end
 
   def show
