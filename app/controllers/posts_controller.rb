0647d746680c9ae8738e91efe5ed01991e5210f9
dai.mugi <dxinchuan@gmail.com>
2021-09-24 14:20:51 +0000

    for heroku update4

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -39,36 +39,6 @@ class PostsController < ApplicationController
     redirect_back(fallback_location: root_path)
   end
   
-  def address 
-    @posts_address = Post.order(@distance: :desc).page(param[:page])
-      @posts_address.each do |pa|
-        
-      lat1 = current_user.latitude
-      lng1 = current_user.longitude
-      lat2 = pa.user.latitude
-      lng2 = pa.user.longitude
-      
-      x1 = lat1.to_f * Math::PI / 180
-      y1 = lng1.to_f * Math::PI / 180
-      x2 = lat2.to_f * Math::PI / 180
-      y2 = lng2.to_f * Math::PI / 180
-      
-      radius = 6378.137
-      
-      diff_y = (y1 - y2).abs
-      
-      calc1 = Math.cos(x2) * Math.sin(diff_y)
-      calc2 = Math.cos(x1) * Math.sin(x2) - Math.sin(x1) * Math.cos(x2) * Math.cos(diff_y)
-      
-      numerator = Math.sqrt(calc1 ** 2 + calc2 ** 2)
-      
-      denominator = Math.sin(x1) * Math.sin(x2) + Math.cos(x1) * Math.cos(x2) * Math.cos(diff_y)
-      
-      degree = Math.atan2(numerator, denominator)
-      
-      @distance = "#{(degree * radius).round(1)} km"
-    end  
-  end
   
   private
 
