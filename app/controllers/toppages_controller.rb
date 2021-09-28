f886a53056ce33d4da0010fef18307f755ed640b
dai.mugi <dxinchuan@gmail.com>
2021-09-28 13:57:59 +0000

    post update6 order distance

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -5,7 +5,8 @@ class ToppagesController < ApplicationController
     if logged_in?
       @posts = Post.order(updated_at: :desc).page(params[:page]).per(10)
       @posts_price = Post.order(price: :desc).page(params[:page]).per(10)
-      @posts_distance = Post.order(updated_at: :desc).page(params[:page]).per(10)
+      @posts_distance = @posts.sort_by { |post| position(post.user).to_f }
+      @posts_distance = Kaminari.paginate_array(@posts_distance).page(params[:page]).per(10)
     end
   end
 end
