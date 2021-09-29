e0e6f0ed4606adf4779fe6496cd4ca691a13b681
dai.mugi <dxinchuan@gmail.com>
2021-09-29 15:10:15 +0000

    pagination update2

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -3,10 +3,10 @@ class ToppagesController < ApplicationController
   def index
     @users = User.order(id: :desc).page(params[:page]).per(12)
     if logged_in?
-      @posts = Post.order(updated_at: :desc).page(params[:page]).per(10)
-      @posts_price = Post.order(price: :desc).page(params[:page]).per(10)
+      @posts = Post.order(updated_at: :desc).page(params[:updated_at_page]).per(10)
+      @posts_price = Post.order(price: :desc).page(params[:price_page]).per(10)
       @posts_distance = @posts.sort_by { |post| position(post.user).to_f }
-      @posts_distance = Kaminari.paginate_array(@posts_distance).page(params[:page]).per(10)
+      @posts_distance = Kaminari.paginate_array(@posts_distance).page(params[:distance_page]).per(10)
     end
   end
 end
