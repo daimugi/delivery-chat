be2cb1b87e94c67d704df1002a6c5d65148d1124
dai.mugi <dxinchuan@gmail.com>
2021-10-14 07:26:55 +0000

    Update post validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,12 @@ class Post < ApplicationRecord
   belongs_to :user
   
   validates :content, presence: true, length: { maximum: 100 }
-  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  # validates :hundred, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 100, less_than_or_equal_to: 5000 }, length: { in: 3..4 }
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 100, less_than_or_equal_to: 5000 }, length: { in: 3..4 }
+  validates :hundred, presence: true
+  private 
+  
+  def hundred
+    price.to_i % 100 == 0
+  end  
 end
